1c835e840b5fe6efe4e048d8159fb71fa25f8eff
LuK1337 <priv.luk@gmail.com>
2020-02-17 15:22:19 +0100

    pioneer: Migrate to custom SPL/android version props
    sed -i 's/ro.build.version.release/ro.twrp.version.release\x00/g' android.hardware.keymaster@3.0-impl-qti.so
    sed -i 's/ro.build.version.security_patch/ro.twrp.version.security_patch\x00/g' android.hardware.keymaster@3.0-impl-qti.so
    
    Change-Id: I9e5cf2a13635a5a301c0329f2390fe0ac54eeaca

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -20,17 +20,17 @@ mount -t ext4 -o ro "$syspath" /s
 
 is_fastboot_twrp=$(getprop ro.boot.fastboot)
 if [ ! -z "$is_fastboot_twrp" ]; then
-    setprop ro.build.version.release "$(getprop ro.build.version.release_orig)"
-    setprop ro.build.version.security_patch "$(getprop ro.build.version.security_patch_orig)"
+    setprop ro.twrp.version.release "$(getprop ro.build.version.release)"
+    setprop ro.twrp.version.security_patch "$(getprop ro.build.version.security_patch)"
     finish
 fi
 
 if [ -f /s/system/build.prop ]; then
-    setprop ro.build.version.release "$(grep -i 'ro.build.version.release' /s/system/build.prop | cut -f2 -d'=')"
-    setprop ro.build.version.security_patch "$(grep -i 'ro.build.version.security_patch' /s/system/build.prop  | cut -f2 -d'=')"
+    setprop ro.twrp.version.release "$(grep -i 'ro.build.version.release' /s/system/build.prop | cut -f2 -d'=')"
+    setprop ro.twrp.version.security_patch "$(grep -i 'ro.build.version.security_patch' /s/system/build.prop  | cut -f2 -d'=')"
     finish
 fi
 
-setprop ro.build.version.release "$(getprop ro.build.version.release_orig)"
-setprop ro.build.version.security_patch "$(getprop ro.build.version.security_patch_orig)"
+setprop ro.twrp.version.release "$(getprop ro.build.version.release)"
+setprop ro.twrp.version.security_patch "$(getprop ro.build.version.security_patch)"
 finish
